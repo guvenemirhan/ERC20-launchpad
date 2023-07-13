c31b94d23ba33bf12cb1c73c0a3a3b999bc43eb9
guvenemirhan <eg.emirhanguven@gmail.com>
2023-07-13 17:05:03 +0300

    license changes

--- contracts/mocks/ERC20.sol
+++ contracts/mocks/ERC20.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-only
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
--- contracts/utils/SignatureChecker.sol
+++ contracts/utils/SignatureChecker.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-only
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
